2b8b8b5942fc3fdae97c18adefe43606ac2921f2
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-26 08:51:55 +0500

    Graphics shader folder now includes "Shaders" folder

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -7,6 +7,7 @@ const std::wstring Graphics::SHADERS_FOLDER = []() {
     std::wstring modulePath = utils::getCurrModuleFullPath();
     auto p = std::filesystem::path(modulePath);
     auto r = p.remove_filename();
+    r.append(L"Shaders\\");
     return r.wstring();
     }();
 Graphics::Graphics(uint32_t w, uint32_t h)
