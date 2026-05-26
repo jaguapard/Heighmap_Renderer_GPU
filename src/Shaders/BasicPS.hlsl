4f07c531eab788570f98006e51ad6ac5034f38e1
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-27 02:12:10 +0500

    Sending world pos to PS, untested

--- src/Shaders/BasicPS.hlsl
+++ src/Shaders/BasicPS.hlsl
@@ -1,4 +1,4 @@
-float4 main(uint id : SV_PrimitiveID) : SV_Target
+float4 main(float3 worldPos : WorldPos, uint id : SV_PrimitiveID) : SV_Target
 {
     float fid = id;
     return float4(fmod(fid / 21, 1), fmod(fid / 53, 1), fmod(fid / 93, 1), 1.f);
--- src/Shaders/BasicVS.hlsl
+++ src/Shaders/BasicVS.hlsl
@@ -4,9 +4,17 @@ cbuffer CBuf
 	matrix transform;
     float time;
 }
-float4 main(float2 pos : Pos) :  SV_Position
+struct VSOut
+{
+    float4 transformedPos : SV_Position;
+    float3 originalWorldPos : WorldPos;
+};
+VSOut main(float2 pos : Pos)
 {
     float func = sin(pos.x/600+time/6) * cos(pos.y/300+time/3) * (pos.x + pos.y)/3;
     func *= sin(time / 2);
-	return mul(float4(pos.x, func, pos.y, 1.f), transform);
+    VSOut ret;
+    ret.originalWorldPos = float3(pos.x, func, pos.y);
+    ret.transformedPos = mul(float4(ret.originalWorldPos, 1.f), transform);
+    return ret;
 }
