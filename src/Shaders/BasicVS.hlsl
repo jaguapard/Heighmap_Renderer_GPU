987cc185563a2b8f2aa65c1befc6558689262a22
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-26 14:38:43 +0500

    More interesting surface

--- src/Shaders/BasicVS.hlsl
+++ src/Shaders/BasicVS.hlsl
@@ -5,6 +5,6 @@ cbuffer CBuf
 }
 float4 main(float2 pos : Pos) :  SV_Position
 {
-    float func = sin(pos.x/600) * cos(pos.y/300) * sqrt(pos.x * pos.x + pos.y * pos.y); //TODO: calcuate function value
+    float func = sin(pos.x/600) * cos(pos.y/300) * (pos.x + pos.y);
 	return mul(float4(pos.x, func, pos.y, 1.f), transform);
 }
