be134a94ee4613746696142a53f8bb75d5789ff0
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-26 13:44:52 +0500

    Increased fly speed: 5 -> 500

--- src/Game.h
+++ src/Game.h
@@ -19,7 +19,7 @@ private:
 	double gameTime = 0;
 	double globalTime = 0;
 
-	float flySpeed = 5;
+	float flySpeed = 500;
 	UINT vertexCount;
 	DirectX::XMVECTOR camPos, camAng;
 	Microsoft::WRL::ComPtr<ID3D11VertexShader> basicVS;
