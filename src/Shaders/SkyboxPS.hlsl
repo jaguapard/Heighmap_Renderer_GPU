b1a983d7bb00e24afb31e185c467b15c5dc502e8
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-28 15:49:48 +0500

    Working "we have skybox at home" (huge cube subject to perspective transform at origin for now)

--- src/Shaders/SkyboxPS.hlsl
+++ src/Shaders/SkyboxPS.hlsl
@@ -1,8 +1,8 @@
 
 TextureCube tex;
 SamplerState sam;
-float4 main(uint id : SV_PrimitiveID, float2 uv : TEXCOORD0) : SV_Target
+float4 main(uint id : SV_PrimitiveID, float3 worldPos : TEXCOORD0) : SV_Target
 {
-    float4 sample = tex.Sample(sam, float3(uv, 1));
+    float4 sample = tex.Sample(sam, worldPos);
     return sample;
 }
--- src/Shaders/SkyboxVS.hlsl
+++ src/Shaders/SkyboxVS.hlsl
@@ -3,13 +3,13 @@
 struct VSOut
 {
     float4 pos : SV_Position;
-    float2 uv : TEXCOORD0;
+    float3 worldPos : TEXCOORD0;
 };
 VSOut main(float3 pos : Pos, float2 uv: UV)
 {
     VSOut vso;
     vso.pos = mul(float4(pos, 1), viewProjection);
-    vso.uv = uv;
+    vso.worldPos = pos;
     //TODO: force w to 0 in pos mult, and uncomment:
     //vso.pos.z = vso.pos.w; //Or not? Reverse z-buffer! infinity? Force 0 depth after perspecive divide. Z_NEAR?
     return vso;
