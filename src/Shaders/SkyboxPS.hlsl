cc07dfbbbb729188ba16cbaa2ae1e9107f4dab35
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-28 16:22:17 +0500

    Light drawing in the sky

--- src/Shaders/SkyboxPS.hlsl
+++ src/Shaders/SkyboxPS.hlsl
@@ -1,8 +1,14 @@
+#include "cbuff.hlsli"
 
 TextureCube tex;
 SamplerState sam;
+
+static const float SUN_RADIUS_RADIANS = radians(1);
 float4 main(uint id : SV_PrimitiveID, float3 worldPos : TEXCOORD0) : SV_Target
 {
+    float3 dir = normalize(worldPos);
+    float d = dot(dir, normalize(lightDir.xyz));
+    if (d >= cos(SUN_RADIUS_RADIANS)) return float4(1,1,1,1);
     float4 sample = tex.Sample(sam, worldPos);
     return sample;
 }
