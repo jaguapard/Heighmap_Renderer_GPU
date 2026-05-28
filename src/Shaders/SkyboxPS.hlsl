b11b5533ac47aedcffee0d3c5367573c32202c39
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-28 14:06:48 +0500

    Different colors for cube triangles

--- src/Shaders/SkyboxPS.hlsl
+++ src/Shaders/SkyboxPS.hlsl
@@ -1,5 +1,13 @@
 
-float4 main() : SV_Target
+float4 main(uint id : SV_PrimitiveID) : SV_Target
 {
-    return float4(1, 0, 1, 1);
+    float r = id & 1;
+    float g = id & 2;
+    float b = id & 4;
+    float3 cl;
+    if (r == 0 && g == 0 && b == 0) cl = float3(1,1,1);
+    else cl = float3(r,g,b);
+    
+    cl *= (id + 1) / 25.f;
+    return float4(cl, 1);
 }
